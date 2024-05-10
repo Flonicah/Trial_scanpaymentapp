05d9adc34cfb0ed18d1d03d5277b35fdbf6dba8b
Flonicah <bsc-com-28-19@unima.ac.mw>
2024-05-10 21:22:50 +0200

    first attempt scan button

---
+++ app/src/main/java/com/example/trial/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.trial;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+      Button btn_scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_scan=findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(V->
+                {
+                    scanCode();
+                }
+
+                );
+    }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barlaucher.launch(options);
+    }
+    ActivityResultLauncher<ScanOptions> barlaucher = registerForActivityResult(new ScanContract(), result->{
+    if(result.getContents() != null)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("result");
+        builder.setMessage(result.getContents());
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+               dialogInterface.dismiss();
+            }
+        }).show();
+    }
+});
+
+        }
